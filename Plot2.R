40b557903d766c6dfa18b684952889c40ba5a767
royley <stephenjroyle@gmail.com>
2016-11-05 15:16:41 +0000

    Plot 2

---
+++ Plot2.R
@@ -0,0 +1,17 @@
+#Read data source - assuming the source file is saved in the working directory
+  data <- read.csv("./household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+  data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+
+#Subset data
+  data2 <- subset(data, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+
+#Convert dates
+  datetime <- paste(as.Date(data2$Date), data2$Time)
+  data2$Datetime <- as.POSIXct(datetime)
+
+#Create Plot 2
+  plot(data2$Global_active_power~data2$Datetime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+
+#Save plot
+  dev.copy(png, file="plot2.png", height=480, width=480)
+  dev.off()
